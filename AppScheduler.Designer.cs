adfcbba4b2a7df416c2413a7139cca02a47f8c02
cetio <qubbinyt@gmail.com>
2023-09-02 19:44:55 -0400

    Add project files.

---
+++ AppScheduler.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace AppScheduler
+{
+    partial class AppScheduler
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppScheduler));
+            button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            notifyIcon1 = new NotifyIcon(components);
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Add Files";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Multiselect = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 56);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(279, 357);
+            textBox1.TabIndex = 1;
+            textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(297, 56);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(73, 357);
+            textBox2.TabIndex = 2;
+            textBox2.WordWrap = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(376, 56);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(157, 357);
+            textBox3.TabIndex = 3;
+            textBox3.WordWrap = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Files";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(297, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Time";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(376, 38);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Cmd Args";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "App Scheduler";
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(111, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(93, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Pause";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Checked = true;
+            checkBox1.CheckState = CheckState.Checked;
+            checkBox1.Location = new Point(210, 15);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(221, 19);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "Use Exact MS (disable if not working)";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // AppScheduler
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(545, 424);
+            Controls.Add(checkBox1);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "AppScheduler";
+            Text = "App Scheduler";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private NotifyIcon notifyIcon1;
+        private Button button2;
+        private CheckBox checkBox1;
+    }
+}
---
+++ AppScheduler.cs
@@ -0,0 +1,106 @@
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+
+namespace AppScheduler
+{
+    public partial class AppScheduler : Form
+    {
+        private bool _selecting = false;
+
+        public AppScheduler()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            _selecting = true;
+
+            foreach (string file in openFileDialog1.FileNames)
+            {
+                if (string.IsNullOrEmpty(file) || file == "openFileDialog1")
+                    break;
+
+                try
+                {
+                    textBox1.Text += $"{file}{Environment.NewLine}";
+                    textBox2.Text += $"{Interaction.InputBox($"What would you like the time for {file} activation to be (this can be changed later)", "App Scheduler")}{Environment.NewLine}";
+                    textBox3.Text += $"{Environment.NewLine}";
+                }
+                catch
+                {
+                    MessageBox.Show("File time selection prompt was prematurely closed and has defaulted to time 8:00");
+                    textBox1.Text += $"{file}{Environment.NewLine}";
+                    textBox2.Text += $"{"8:00 AM"}{Environment.NewLine}";
+                    textBox3.Text += $"{Environment.NewLine}";
+                }
+            }
+
+            _selecting = false;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+
+            if (_selecting)
+                return;
+
+            foreach (string file in textBox1.Lines)
+            {
+                if (string.IsNullOrEmpty(file))
+                    continue;
+
+                if (!Path.IsPathFullyQualified(file))
+                    throw new ArgumentException($"File path {file} cannot be parsed, and is not valid.");
+
+                if (textBox2.Lines.Length <= Array.IndexOf(textBox1.Lines, file))
+                    continue;
+
+                if (!DateTime.TryParse(textBox2.Lines[Array.IndexOf(textBox1.Lines, file)], out DateTime time))
+                    continue;
+
+                Debug.WriteLine(time.ToString("HH:mm:ss:f"));
+                Debug.WriteLine(DateTime.Now.ToString("HH:mm:ss:f"));
+
+                if ((checkBox1.Checked && DateTime.Now.ToString("HH:mm:ss:f") == time.ToString("HH:mm:ss:f")) ||
+                    (!checkBox1.Checked && DateTime.Now.ToString("HH:mm:ss") == time.ToString("HH:mm:ss")))
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.UseShellExecute = true;
+                    proc.StartInfo.FileName = file;
+                    proc.StartInfo.Arguments = textBox3.Lines.Length > Array.IndexOf(textBox1.Lines, file)
+                        ? textBox3.Lines[Array.IndexOf(textBox1.Lines, file)]
+                        : string.Empty;
+
+                    proc.Start();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                button2.Text = "Resume";
+            }
+            else
+            {
+                timer1.Start();
+                button2.Text = "Pause";
+            }
+        }
+    }
+}
